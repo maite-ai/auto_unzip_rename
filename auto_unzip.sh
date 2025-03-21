883cd3683ef1f0547581abf705259b9adbaf782b
M. Fernanda Rios <64336818+maite-ai@users.noreply.github.com>
2025-03-20 22:33:29 -0300

    fix: remove the read command line since it didn't work

--- auto_unzip.sh
+++ auto_unzip.sh
@@ -11,72 +11,66 @@ GREEN='\033[0;32m'
 
 # Otros colores
 BLUE='\033[0;34m'
-NOCOLOR='\033[0m' # para resetear color
+RESET='\033[0m' # para resetear color y estilo
 
 # Funciones para imprimir con colorcito ;)
 log_info() {
-  echo -e "${BLUE}[INFO]${NOCOLOR} $1"
+  echo -e "${BLUE}[INFO]${RESET} $1"
 }
 
 log_error() {
-  echo -e "${RED}[ERROR] $1${NOCOLOR}"
+  echo -e "${RED}[ERROR] $1${RESET}"
 }
 
 log_warning() {
-  echo -e "${YELLOW}[WARNING]${NOCOLOR} $1"
+  echo -e "${YELLOW}[WARNING]${RESET} $1"
 }
 
 log_success() {
-  echo -e "${GREEN}[SUCCESS] $1${NOCOLOR}"
+  echo -e "${GREEN}[SUCCESS] $1${RESET}"
 }
 
 # Crea una lista con los distintos challenges de la unidad
 folder_name=$(basename "$(pwd)")
-echo -e "${UNDERLINE}Obteniendo lista de challenges de la unidad ${folder_name}${NOCOLOR}"
+echo -e "${UNDERLINE}Obteniendo lista de challenges de la unidad ${folder_name}${RESET}"
 challenges=(*)
 
 # Itero sobre la lista
 for challenge in "${challenges[@]}"; do
   if [ -d "$challenge" ]; then # verifica que sea un directorio
     cd $challenge
-    log_info "Ingresando al challenge ${BOLD}${challenge}${NOCOLOR}"
+    log_info "Ingresando al challenge ${BOLD}${challenge}${RESET}"
 
     if ls | grep -q ".zip"; then
       zip_files=$(find . -maxdepth 1 -type f -name '*.zip')
 
       for zip_file in $zip_files; do
-        log_info "Encontrado archivo: ${BOLD}${zip_file}${NOCOLOR}"
+        log_info "Encontrado archivo: ${BOLD}${zip_file}${RESET}"
 
         unzip_output=$(unzip -l "$zip_file")
         echo "$unzip_output"
 
-        read -p "¿Deseas descomprimir este archivo? (s/n): " extract_choice
+        # creo carpeta temporal donde va a estar almacenado el ipynb
+        temp_dir=$(mktemp -d)
+        log_info "Creando directorio temporal: ${temp_dir}"
+        unzip -q "$zip_file" -d "$temp_dir"
 
-        if [[ "$extract_choice" == "s" ]]; then
-          # creo carpeta temporal donde va a estar almacenado el ipynb
-          temp_dir=$(mktemp -d)
-          log_info "Creando directorio temporal: ${temp_dir}"
-          unzip -q "$zip_file" -d "$temp_dir"
-
-          solution_ipynb=$(find "$temp_dir" -type f -name "*.ipynb" | head -1)
-          if [[ -z "$solution_ipynb" ]]; then
-            log_warning "No se encontró archivo ipynb de solución en el zip"
-            rm -rf "$temp_dir"
-            continue
-          fi
-
-          solution_ipynb_name=$(basename "$solution_ipynb")
-          log_info "Archivo ipynb de solución encontrado: ${solution_ipynb_name}"
-
-          cp "$solution_ipynb" "./sol_$solution_ipynb_name"
-
-          # borrar el directorio temporal
+        solution_ipynb=$(find "$temp_dir" -type f -name "*.ipynb" | head -1)
+        if [[ -z "$solution_ipynb" ]]; then
+          log_warning "No se encontró archivo ipynb de solución en el zip"
           rm -rf "$temp_dir"
-          rm "$zip_file"
-          log_success "Archivo .zip eliminado"
-        else
-          log_info "Archivo no procesado"
+          continue
         fi
+
+        solution_ipynb_name=$(basename "$solution_ipynb")
+        log_info "Archivo ipynb de solución encontrado: ${solution_ipynb_name}"
+
+        cp "$solution_ipynb" "./sol_$solution_ipynb"
+
+        # borrar el directorio temporal
+        rm -rf "$temp_dir"
+        rm "$zip_file"
+        log_success "Archivo .zip eliminado"
       done
     else
       log_info "No hay archivos comprimidos en este directorio"
@@ -86,4 +80,4 @@ for challenge in "${challenges[@]}"; do
   fi
 done
 
-echo -e "${BOLD}===== Proceso completado ====="
+echo -e "${BOLD}===== Proceso completado =====${RESET}"
